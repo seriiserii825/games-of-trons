332a09949f30e5b7f6e60fc47d21a90c71ebf3c9
serii <seriiburduja@gmail.com>
2020-08-20 11:21:23 +0300

    charDetails

--- src/components/charDetails/charDetails.js
+++ src/components/charDetails/charDetails.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import "./charDetails.scss";
 
 const CharDetails = () => {
 	return (
-		<div>
-
+		<div className="char-details">
+			<h2 className="char-details__title">John Smith</h2>
+			<ul className="char-details__list">
+				<li className="char-details__item">
+					<span className="char-details__property">Gender</span>
+					<span className="char-details__value">male</span>
+				</li>
+				<li className="char-details__item">
+					<span className="char-details__property">Born</span>
+					<span className="char-details__value">1783</span>
+				</li>
+				<li className="char-details__item">
+					<span className="char-details__property">Died</span>
+					<span className="char-details__value">1820</span>
+				</li>
+			</ul>
 		</div>
 	);
 };
